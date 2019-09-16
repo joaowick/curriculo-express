21f26f4501816211d0ec13df47c38e9f20744c19
Joao Torres <joaotorrez14@gmail.com>
2019-09-16 17:23:46 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+//Importar para nossa aplicação
 const express = require('express');
+const path = require('path');
+
+//Criamos uma constante para a porta pela qual as pessoas poderão acessar nosso servidor
 const port = 3000;
+
+//Instaciamos uma aplicação Express
 const app = express();
 
+//Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res, next) => {
-    res.send({
+    res.render('index', {
         title: "Meu primeiro servidor Express",
         version: "0.0.0"
     });
